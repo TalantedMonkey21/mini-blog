c2ee88c001347308ce1faee03dc76de7ddc693c4
Maxim Sherstneev <m.sherstneev@yandex.ru>
2025-09-19 02:07:48 +0300

    fix

--- blog_project/blog_project/settings.py
+++ blog_project/blog_project/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-rej!86r81p%av0ocgf-3u^khb1wpm^%nl#_663c2!1fdbw@_8s
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["158.160.109.66", "localhost", "127.0.0.1"]
 
 
 # Application definition
@@ -128,7 +128,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = "/static/"
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
