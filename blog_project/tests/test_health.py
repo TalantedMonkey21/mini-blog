ae2c96db92c47afd207f45d7923770377b650256
Maxim Sherstneev <m.sherstneev@yandex.ru>
2025-10-01 00:49:42 +0300

    Test 3

--- blog_project/tests/test_health.py
+++ blog_project/tests/test_health.py
@@ -3,5 +3,5 @@ from django.test import TestCase, Client
 class HealthzTest(TestCase):
     def test_healthz(self):
         c = Client()
-        resp = c.get("/healthz")
+        resp = c.get("/healthz/")
         assert resp.status_code in (200, 204)
